69de4d0a1bd92fbf5b2761b4419caa00b16fbd73
vermasuraj11011 <vermasuraj0008@gmail.com>
2023-12-17 22:34:53 +0530

    adding day-8

---
+++ day-8/function.py
@@ -0,0 +1,12 @@
+def great(name):
+    print(f"hello {name}")
+
+
+great("suraj")
+
+
+def multiParamGreet(fromPerson, to):
+    print(f"hello from {fromPerson} to {to}")
+
+
+multiParamGreet(fromPerson="suraj", to="shyam")
