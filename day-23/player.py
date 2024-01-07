f01528c5f58721dd323d5c0cc0d6c9bb625f1003
Suraj Verma <vermasuraj0008@gmail.com>
2024-01-07 22:10:42 +0530

    adding day-23 completed

---
+++ day-23/player.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape('turtle')
+        self.penup()
+        self.start()
+        self.setheading(90)
+
+    def go_up(self):
+        self.forward(20)
+
+    def start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        return self.ycor() > FINISH_LINE_Y
